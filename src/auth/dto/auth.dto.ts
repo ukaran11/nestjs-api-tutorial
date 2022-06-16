b2348eb82f1646b8d0d4569a61bbd176e2c00de7
unknown <ukaran11@gmail.com>
2022-06-16 22:58:21 +0530

    Utsav | Added dto with class validator

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,11 @@
-export interface AuthDto {
+// import { IsNotEmpty, IsString, IsEmail} from 'class-validator';
+import {IsEmail, IsNotEmpty, IsString} from  'class-validator';
+export class AuthDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
