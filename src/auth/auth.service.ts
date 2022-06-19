2814d6753a0c1948a1a5a0b2f69c82c8b163c785
unknown <ukaran11@gmail.com>
2022-06-19 20:25:57 +0530

    Utsav | Changed the response to a json

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -61,10 +61,10 @@ export class AuthService {
     return this.signToken(user.id, user.email);
   }
 
-  signToken(
+  async signToken(
     userId: number,
     email: string
-  ): Promise<String> {
+  ): Promise<{access_token: string}> {
     const payload = {
       sub: userId,
       email
@@ -72,9 +72,11 @@ export class AuthService {
 
     const secret = this.config.get('JWT_SECRET');
 
-    return this.jwt.signAsync(payload, {
+    const token = await this.jwt.signAsync(payload, {
       expiresIn: '15m',
       secret: secret
-    })
+    });
+
+    return { access_token: token}
   }
 }
