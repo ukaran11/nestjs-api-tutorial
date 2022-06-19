1128a86c765a48a9fcac35bd2778fb7995a9fc1d
unknown <ukaran11@gmail.com>
2022-06-19 19:28:42 +0530

    Utsav | Added token signing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,9 +3,16 @@ import * as argon from 'argon2';
 import { PrismaService } from "src/prisma/prisma.service";
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { AuthDto } from "./dto";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService
+    ) {}
+
   async signup(dto: AuthDto) {
     // generate the password hash
     const hash = await argon.hash(dto.password);
@@ -18,8 +25,8 @@ export class AuthService {
           hash
         }
       })
-      delete user.hash;
-      return user;
+
+      return this.signToken(user.id, user.email);
     } catch(error) {
       if(error instanceof PrismaClientKnownRequestError) {
         if(error.code === 'P2002') {
@@ -51,10 +58,23 @@ export class AuthService {
       throw new ForbiddenException('Credentials Incorrect');
     }
 
-    // Send back the user
-    delete user.hash;
-    return user;
+    return this.signToken(user.id, user.email);
   }
 
+  signToken(
+    userId: number,
+    email: string
+  ): Promise<String> {
+    const payload = {
+      sub: userId,
+      email
+    }
 
+    const secret = this.config.get('JWT_SECRET');
+
+    return this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret
+    })
+  }
 }
